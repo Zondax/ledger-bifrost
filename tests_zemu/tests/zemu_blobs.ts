a6b4d0faf18fae6ddee8f248cb22b6c598c08aad
Carlos Medeiros <104493454+chcmedeiros@users.noreply.github.com>
2022-06-02 15:00:52 +0100

    Initial commit

---
+++ tests_zemu/tests/zemu_blobs.ts
@@ -0,0 +1,19 @@
+/** ******************************************************************************
+ *  (c) 2019 - 2022 Zondax GmbH
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************* */
+
+export const txBalances_transfer =
+  '0a0000313233343536373839303132333435363738393031323334353637383930313233158139ae28a3dfaac5fe1560a5e9e05cd5038d2433158139ae28a3dfaac5fe1560a5e9e05ca403000001000000262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b'
+
